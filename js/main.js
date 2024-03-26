287baf5563832e7b809a5c22250f510c4fc5ed48
Ryan Leite <ryan.aleite@yahoo.com.br>
2024-03-26 16:53:08 -0300

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,43 @@
+$(document).ready(function(){
+
+  $('#telefone').mask('(00) 00000-0000');
+
+  // $('form').validate({
+  //   rules: {
+  //     nome: {
+  //       required: true
+  //     },
+  //     email: {
+  //       required: true,
+  //       email: true
+  //     },
+  //     telefone: {
+  //       required: true
+  //     }, 
+  //     cpf: {
+  //       required: true
+  //     }, 
+  //     endereco: {
+  //       required: true
+  //     }, 
+  //     cep: {
+  //       required: true
+  //     } 
+  //   },
+  //   messages: {
+  //     nome: 'Por favor, insira o seu nome',
+  //     telefone: 'Por favor, insira seu telefone',
+  //     email: 'Por favor, insira um email válido',
+  //     cpf: 'Por favor, insira seu CPF',
+  //     endereco: 'Por favor, insira seu endereço',
+  //     cep: 'Por favor, insira seu CEP'
+  //   },
+  //   submitHandler: function(form) {
+  //     console.log(form)
+  //   },
+  //   invalidHandler: function(evento, validador) {
+  //     let camposIncorretos = validador.numberOfInvalids();
+  //     console.log();
+  //   }
+  // })
+})
