a99775b8132da131b27f4c81956f091602c0c9c1
Ryan Leite <ryan.aleite@yahoo.com.br>
2024-05-14 14:14:33 -0300

    Fix user login

--- js/login-cliente.js
+++ js/login-cliente.js
@@ -4,7 +4,7 @@ $('#form-login-cliente').on('submit', e => {
   const senha = $('#senha').val()
 
   $.ajax({
-    url: 'https://pit2-api.pd8edx.easypanel.host/usuario/cliente/login',
+    url: 'https://pit2-api.pd8edx.easypanel.host/api/usuario/cliente/login',
     method: 'post',
     data: { email, senha },
     dataType: 'json',
