3b88946c9f23d79207ab21ff2078ccd1de9ebc9f
Ryan Leite <ryan.aleite@yahoo.com.br>
2024-05-21 18:01:19 -0300

    Fixed register screens, created functions to request diet

--- js/request-diet.js
+++ js/request-diet.js
@@ -1,6 +1,15 @@
 $(document).on('ready', async function() { 
+  $('#data-nasc').mask('00/00/0000', {placeholder: 'dd/mm/aaaa' });
   const dadosUsuario = JSON.parse(localStorage.getItem('dadosUsuario'))
   $('#nome').val(`${dadosUsuario.nome} ${dadosUsuario.sobrenome}`)
+  
+  if(`${dadosUsuario.genero}` === 'M'){
+    $('#genero').val("Masculino")
+  } else {
+    $('#genero').val(`${dadosUsuario.genero}`)
+  }
+  
+  $('#data-nasc').val(`${dadosUsuario.dataNascimento}`)
   const listaNutricionistas = await $.ajax({
     url: 'https://pit2-api.pd8edx.easypanel.host/api/nutricionista/'
   })
@@ -11,7 +20,24 @@ $(document).on('ready', async function() {
   })
 })
 
-$(document).on('submit', async function(){
+$('#form-request').on('submit', async function(e){
+  e.preventDefault()
   const dadosUsuario = JSON.parse(localStorage.getItem('dadosUsuario'))
-  
+  const body = {
+    "clienteId": dadosUsuario.cliente.id,
+    "objetivoFoco": $('#tipo-dieta option:selected').text(),
+    "nutricionista": {
+      "nutricionistaId": $('#nutricionistas').val()
+    }
+  }
+  const bodyMedidas =
+    {
+      "clienteId": dadosUsuario.cliente.id,
+      "altura": $('#altura').val(),
+      "peso": $('#peso').val()
+    }
+  const response = await $.post('https://pit2-api.pd8edx.easypanel.host/api/usuario/cliente/solicitardieta',body)
+  alert(response.message)
+  const responseMedidas = await $.post('https://pit2-api.pd8edx.easypanel.host/api/medidas',bodyMedidas)
+  alert(responseMedidas.message)
 })
