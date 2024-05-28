35c812f74420ee74fd885132baf62852538e1ab4
Ryan Leite <ryan.aleite@yahoo.com.br>
2024-05-28 16:55:56 -0300

    Added functions in pendentes-nutritionist (create/send diet), dashboard-user now has graphics and other small changes.

--- js/dashboard-user.js
+++ js/dashboard-user.js
@@ -1,5 +1,53 @@
 $(document).on('ready', async function() { 
   const dadosUsuario = JSON.parse(localStorage.getItem('dadosUsuario'))
+  let progresso = await $.get(`https://pit2-api.pd8edx.easypanel.host/api/medidas/progresso/cliente/${dadosUsuario.cliente.id}`)
+  console.log(progresso.map(progresso => [progresso.dataCriacao, progresso.peso]))
+  progresso = progresso.slice(0,5)
   $('#user-name').text(`Olá, ${dadosUsuario.nome} ${dadosUsuario.sobrenome}!`)
   
-  })
+  google.charts.load('current', {'packages':['corechart']});
+  google.charts.setOnLoadCallback(drawChart);
+  google.charts.setOnLoadCallback(graficoIMC);
+
+  function drawChart() {
+    var data = google.visualization.arrayToDataTable([
+      ['Data', 'Peso'],
+      ...progresso.map(progresso => [new Date(progresso.dataCriacao).toLocaleDateString('pt-BR'), Number(progresso.peso)])
+    ]);
+
+    const chartwidth = $('#grafico').width();  
+    var options = {
+      title: 'Histórico de Peso',
+      curveType: 'function',
+      legend: { position: 'bottom' },
+      width: 700,
+      height: 250, 
+      responsive: true
+    };
+
+    var chart = new google.visualization.LineChart(document.getElementById('grafico'));
+
+    chart.draw(data, options);
+  }
+
+  function graficoIMC() {
+    var data = google.visualization.arrayToDataTable([
+      ['Data', 'IMC'],
+      ...progresso.map(progresso => [new Date(progresso.dataCriacao).toLocaleDateString('pt-BR'), Number(progresso.valorImc)])
+    ]);
+
+    const chartwidth = $('#grafico2').width();  
+    var options = {
+      title: 'Histórico de IMC',
+      curveType: 'function',
+      legend: { position: 'bottom' },
+      width: 700,
+      height: 250, 
+      responsive: true
+    };
+
+    var chart = new google.visualization.LineChart(document.getElementById('grafico2'));
+
+    chart.draw(data, options);
+  }
+})
--- js/pendentes-nutritionist.js
+++ js/pendentes-nutritionist.js
@@ -1,15 +1,87 @@
-$(document).on('ready', async function(){
+$(document).on('ready', async function () {
+  let idRegistro = 0
+  let idCliente = 0
   const dadosUsuario = JSON.parse(localStorage.getItem('dadosUsuario'))
-  const clientesPendentes = await $.get(`https://pit2-api.pd8edx.easypanel.host/api/nutricionista/clientes/pendentes/${dadosUsuario.nutricionista.id}`)
+  const listaAlimentos = await $.get('https://pit2-api.pd8edx.easypanel.host/api/alimento/')
 
-  for(const {dieta} of clientesPendentes){
-    const template = $($('#template-cliente').html()).clone()
+  await carregarTabela()
 
-    template.find(".id").text(dieta.cliente.id)
-    template.find(".nome").text(`${dieta.cliente.usuario.nome} ${dieta.cliente.usuario.sobrenome}`)
-    template.find(".objetivoFoco").text(dieta.objetivoFoco)
-    template.find(".solicitacao").text(new Date(dieta.dataCriacao).toLocaleString('pt-BR'))
+  $('#adicionar-registro').on('click', adicionarRegistro)
+  $('#close-modal').on('click', closeModal)
 
-    $("table#tabela-pendentes tbody").append(template)
+  async function modalDieta(dietaId, userId) {
+    if (idCliente !== userId){
+      $('#tabela-alimentos tbody tr').remove()
+    }
+    idCliente = userId
+    const modal = document.getElementById('modal').classList.add('visible')
+    console.log(modal)
+  }
+
+  async function closeModal(){
+    const modal = document.getElementById('modal').classList.remove('visible')
+  }
+
+  $('#enviar-dieta').on('click', async function () {
+    const alimentos = []
+    $('#tabela-alimentos tbody tr').map((_, value) => {
+      const registroAlimento = {
+        alimentoId: $(value).find('.select-options').val(),
+        horario: new Date(`1970-01-01T${$(value).find('.horario').val()}:00Z`).toISOString(),
+        pesoGramas: $(value).find('.quantidade').val()
+      }
+
+      alimentos.push(registroAlimento)
+    })
+
+    const bodyModal = {
+      clienteId: idCliente,
+      objetivoFoco: 'Mudar de vida',
+      dietaAtual: true,
+      alimentos: alimentos,
+      nutricionista: {
+        nutricionistaId: dadosUsuario.nutricionista.id
+      }
+    }
+    const response = await $.post(`https://pit2-api.pd8edx.easypanel.host/api/dieta/`, bodyModal)
+    alert(response.message)
+  })
+
+  async function adicionarRegistro() {
+    const template = $($('#template-dieta').html()).clone()
+    $(template).attr('id', `linha-${idRegistro++}`)
+    await carregarAlimentos($(template).find('select#select-options'))
+    $(template).find('.excluir').on('click', excluirLinha)
+    $('#tabela-alimentos tbody').append(template)
+  }
+
+  function excluirLinha(e) {
+    $(e.target).parents('tr').remove()
+  }
+
+  async function carregarTabela() {
+    const clientesPendentes = await $.get(
+      `https://pit2-api.pd8edx.easypanel.host/api/nutricionista/clientes/pendentes/${dadosUsuario.nutricionista.id}`
+    )
+    for (const { dieta } of clientesPendentes) {
+      const template = $($('#template-cliente').html()).clone()
+
+      template.find('.id').text(dieta.cliente.id)
+      template.find('.nome').text(`${dieta.cliente.usuario.nome} ${dieta.cliente.usuario.sobrenome}`)
+      template.find('.objetivoFoco').text(dieta.objetivoFoco)
+      template.find('.solicitacao').text(new Date(dieta.dataCriacao).toLocaleString('pt-BR'))
+      template
+        .find('.send-diet')
+        .attr('diet-id', dieta.id)
+        .on('click', _ => modalDieta(dieta.id, dieta.cliente.id))
+      $('table#tabela-pendentes tbody').append(template)
+    }
+  }
+
+  async function carregarAlimentos(seletor) {
+    $(seletor).append(`<option value=''>Escolha seu alimento...</option>`)
+    for (const alimento of listaAlimentos) {
+      $(seletor).append(`<option value='${alimento.id}'>${alimento.nome}</option>`)
+    }
   }
 })
