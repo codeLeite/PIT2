6467032fd8d883cf92fb2ac61b6e81c32c5d7664
Ryan Leite <146973569+codeLeite@users.noreply.github.com>
2024-07-02 19:44:41 -0300

    Add files via upload

---
+++ js/anamnese-user.js
@@ -0,0 +1,10 @@
+document.on("ready", async () => {
+    $("form").on("submit", async () => {
+        const dadosUsuario = JSON.parse(localStorage.getItem(("dadosUsuario")))
+        const checkedForms = $("input[value=true]:checked").length
+        const tipoPerfil = checkedForms > 3 ? "Perfil Especial" : "Perfil Normal";
+
+        $.put(`https://pit2-api.pd8edx.easypanel.host/api/usuario/cliente/`, {id: dadosUsuario.cliente.id, cliente: {tipoPerfil}})
+
+    })
+})
