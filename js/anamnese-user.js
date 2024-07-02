a1ee427b6c600cd32edfe678cc8e7998fdaa3abd
Ryan Leite <146973569+codeLeite@users.noreply.github.com>
2024-07-02 19:46:53 -0300

    Add files via upload

--- js/anamnese-user.js
+++ js/anamnese-user.js
@@ -5,6 +5,8 @@ document.on("ready", async () => {
         const tipoPerfil = checkedForms > 3 ? "Perfil Especial" : "Perfil Normal";
 
         $.put(`https://pit2-api.pd8edx.easypanel.host/api/usuario/cliente/`, {id: dadosUsuario.cliente.id, cliente: {tipoPerfil}})
-
+        alert('Perfil de anamnese enviado!')
+        window.location.href = './dashboard-user.html'
     })
+
 })
